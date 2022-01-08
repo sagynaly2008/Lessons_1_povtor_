61e80a9508ae8f4965769b7b973fc3402c70df85
user <sagynalytiirov@gmail.com>
2022-01-08 19:22:55 +0600

    Initial commit

---
+++ app/src/main/java/com/example/lessons_1_povtor/MainActivity.java
@@ -0,0 +1,23 @@
+package com.example.lessons_1_povtor;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.ImageView;
+
+import com.bumptech.glide.Glide;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ImageView image;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        image=findViewById(R.id.image);
+        Glide.with(this).load("https://i.pinimg.com/474x/23/ab/a6/23aba60b66ef08174bb7455c4a8a2d2f.jpg").into(image);
+
+    }
+}
